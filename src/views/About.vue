46c16421378615242ed416df3cf5c59f6bc6ce6a
rconde <conde.rosario88@gmail.com>
2020-11-05 19:51:15 -0300

    mapState

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
     <h1>Ruta protegida</h1>
+    {{token}}
   </div>
 </template>
+<script>
+import { mapState } from "vuex";
+export default {
+  computed:{
+    ...mapState(['token'])
+  }
+};
+</script>

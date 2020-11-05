8b2dc8c0756fab75ad1a782c07987983f3557627
rconde <conde.rosario88@gmail.com>
2020-11-05 19:50:15 -0300

    vuex login obtenerToken

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,41 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    token: null
   },
   mutations: {
+    setToken(state, payload) {
+      state.token = payload
+    }
   },
   actions: {
+    async login({ commit }, usuario) {
+      console.log(usuario);
+      try {
+        const res = await fetch('http://localhost:3001/api/user/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(usuario)
+        })
+        const resDB = await res.json()
+        console.log(resDB.data.token)
+
+        commit('setToken', resDB.data.token)
+
+        localStorage.setItem('token', resDB.data.token)
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    obtenerToken({ commit }) {
+      if (localStorage.getItem('token')) {
+        commit('setToken', localStorage.getItem('token'))
+      } else {
+        commit('setToken', null)
+      }
+    }
   },
   modules: {
   }
